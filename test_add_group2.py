0fd432b29b1ced5836d8175be7aaca979f730559
Olga_177 <1774659@gmail.com>
2018-07-03 22:12:45 -0700

    First scenario with extracted methods

--- test_add_group2.py
+++ test_add_group2.py
@@ -18,11 +18,41 @@ class test_add_group2(unittest.TestCase):
         self.wd.implicitly_wait(60)
 
     def test_test_add_group2(self):
-        success = True
         wd = self.wd
-        # Open Home page
-        wd.get("http://localhost/addressbook/")
-        # Login
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.create_group(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # Logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
+        # Initiating group creation
+        wd.find_element_by_name("new").click()
+        # Populate group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("group_name")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("group_header")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("group_footer")
+        # Submitting group form
+        wd.find_element_by_name("submit").click()
+
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -31,47 +61,12 @@ class test_add_group2(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        # Open group page
-        wd.find_element_by_link_text("groups").click()
-        # Add new group
-        wd.find_element_by_name("new").click()
-        # Populate group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group_name")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("group_header")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("group_footer")
-        # Submitting group form
-        wd.find_element_by_name("submit").click()
-        # Open group page
-        wd.find_element_by_link_text("groups").click()
-        # Add new group
-        wd.find_element_by_name("new").click()
-        # Populate group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group_name")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("group_header")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("group_footer")
-        # Submitting group form
-        wd.find_element_by_name("submit").click()
-        # Open group page
-        wd.find_element_by_link_text("groups").click()
-        # Logout
-        wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
-
 
 if __name__ == '__main__':
     unittest.main()
