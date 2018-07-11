cdead5ae81c8f52624e6b1c9b09b4480a4224afd
Olga_177 <1774659@gmail.com>
2018-07-11 16:12:55 -0700

    test_edit_contact updated

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -5,7 +5,7 @@ def test_edit_first_contact(app):
     if (app.contact.count_contacts() == 0):
         app.contact.add_contact(Contact(first_name="first_name", last_name="last_name",
                                         address="address", mobile_phone="mobile_phone", email="email"))
-    else:
-        app.contact.edit_first_contact(Contact(first_name="new_first_name", last_name="new_last_name",
-                                               address="new_address", mobile_phone="new_mobile_phone",
-                                               email="new_email"))
+
+    return app.contact.edit_first_contact(Contact(first_name="new_first_name", last_name="new_last_name",
+                                                  address="new_address", mobile_phone="new_mobile_phone",
+                                                  email="new_email"))
