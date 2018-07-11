805f8587c835e8af2fee29da122948a88dfd42bc
Olga_177 <1774659@gmail.com>
2018-07-11 12:57:14 -0700

    Assert with list comparision is added to test_add_group

--- test/test_add_group2.py
+++ test/test_add_group2.py
@@ -2,8 +2,10 @@ from model.group import Group
 
 
 def test_add_group2(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group_name", header="group_header", footer="group_footer"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len (new_groups)
 
 def test_add_empty_group2(app):
     app.group.create(Group(name="", header="", footer=""))
