c5fd4fe4ad77daa55b14339c245e77cf198bece9
Olga_177 <1774659@gmail.com>
2018-07-10 18:58:12 -0700

    Delete contacts updated

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,7 +2,8 @@ from model.contact import Contact
 
 
 def test_delete_first_contact(app):
-    if app.contact.count_contacts() == 0:
+    if (app.contact.count_contacts() == 0):
         app.contact.add_contact(Contact(first_name="first_name", last_name="last_name",
                                         address="address", mobile_phone="mobile_phone", email="email"))
-    app.contact.delete_first_contact()
+    else:
+        app.contact.delete_first_contact()
