b9088041c28b3d919d5d96aaf74051d9b2e47c33
Olga_177 <1774659@gmail.com>
2018-07-11 20:21:07 -0700

    List comparison is added to contacts (add, delete, edit)

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,11 +1,9 @@
 from model.contact import Contact
+from fixture.contact import *
 
 
 def test_edit_first_contact(app):
-    if (app.contact.count_contacts() == 0):
-        app.contact.add_contact(Contact(first_name="first_name", last_name="last_name",
-                                        address="address", mobile_phone="mobile_phone", email="email"))
-
-    return app.contact.edit_first_contact(Contact(first_name="new_first_name", last_name="new_last_name",
-                                                  address="new_address", mobile_phone="new_mobile_phone",
-                                                  email="new_email"))
+    old_contacts = app.contact.get_contact_list()
+    app.contact.edit_first_contact(Contact(first_name="first_name5", last_name="last_name5"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
