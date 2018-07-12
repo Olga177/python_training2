50394d12c1b02994b708deb661b2af8b33f7d580
Olga_177 <1774659@gmail.com>
2018-07-11 20:16:31 -0700

    List comparison is added to test_del_contact

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,10 +1,12 @@
 from model.contact import Contact
+from fixture.contact import *
 
 
 def test_delete_first_contact(app):
-    if (app.contact.count_contacts() == 0):
-        app.contact.add_contact(Contact(first_name="first_name", last_name="last_name",
-                                        address="address", mobile_phone="mobile_phone", email="email"))
-
-    return app.contact.delete_first_contact()
+    # if (app.contact.count_contacts() == 0):
+    #     app.contact.add_contact(Contact(first_name="first_name1", last_name="last_name1")
+    old_contacts = app.contact.get_contact_list()
+    app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert (len(old_contacts) - 1) == len(new_contacts)
 
