75a86c4bf7d4aa08275640dfbbd79c54b931df42
Olga_177 <1774659@gmail.com>
2018-07-13 09:10:39 -0700

    Caching for add, delete, modify group is implemented

--- test/test_add_group2.py
+++ test/test_add_group2.py
@@ -7,7 +7,7 @@ def test_add_group2(app):
     group = Group(name="group_name", header="group_header", footer="group_footer")
     app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == app.group.count()
     old_groups.append(group)
     assert sorted(old_groups, key= Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
