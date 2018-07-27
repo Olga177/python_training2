05a4fc3ecc7dcd7369df230ca1e0c4e0103d54bb
Olga_177 <1774659@gmail.com>
2018-07-26 19:14:59 -0700

    Test data for contacts is generated in json format with jsonpickle

--- generator/contact.py
+++ generator/contact.py
@@ -2,9 +2,11 @@ from model.contact import Contact
 import random
 import string
 import os.path
-import json
 import getopt
+import json
 import sys
+import jsonpickle
+
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
@@ -53,4 +55,5 @@ test_data = [Contact(first_name="", last_name="", address="",
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 with open(file, "w") as out:
-    out.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(test_data))
