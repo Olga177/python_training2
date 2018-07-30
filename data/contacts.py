44ee91d881df177f7a82df00465fc1aeb7eca654
Olga_177 <1774659@gmail.com>
2018-07-30 12:49:28 -0700

    Group data loaded from database

---
+++ data/contacts.py
@@ -0,0 +1,15 @@
+from model.contact import Contact
+
+
+
+test_data = [
+    Contact(first_name ='first_name1', last_name = 'last_name1', home_phone = 'home_phone1',
+            mobile_phone='mobile_phone1', work_phone='work_phone1',
+            email1 = 'email11',email2 = 'mail21', email3 = 'email31'),
+    Contact(first_name='first_name2', last_name='last_name2', home_phone='home_phone2',
+            mobile_phone='mobile_phone2', work_phone='work_phone2',
+            email1='email12', email2='mail22', email3='email32')
+]
+
+
+
